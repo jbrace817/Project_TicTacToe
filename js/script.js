9d95c17b75e93a3a6d189144063d227685b5bdc3
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2023-10-06 10:04:43 -0400

    added fn checkForWinner, resetBoard

--- js/script.js
+++ js/script.js
@@ -9,19 +9,18 @@ const gameBoard = (function () {
       const box = boxes[i];
       box.addEventListener("click", (e) => {
         box.innerHTML = playerTurns.switchPlayer();
+        checkForWinner.checkWinner();
       });
     }
   };
-  const resetBoard = () => {
-    for (let i = 0; i < boxes.length; i++) {
-      const box = boxes[i];
-      box.innerHTML = "";
-    }
-  };
+  //   const resetBoard = () => {
+  //     for (let i = 0; i < boxes.length; i++) {
+  //       const box = boxes[i];
+  //       box.innerHTML = "";
+  //     }
+  //   };
 
-  //   const [...test] = boxes;
-
-  return { markBox, resetBoard };
+  return { boxes, markBox };
 })();
 
 const playerTurns = (function () {
@@ -40,10 +39,48 @@ const playerTurns = (function () {
   return { switchPlayer };
 })();
 
-const checkWinner = (function () {})();
+const checkForWinner = (function () {
+  const checkWinner = () => {
+    const [box1, box2, box3, box4, box5, box6, box7, box8, box9] =
+      gameBoard.boxes;
+    const winBoxes = [
+      [box1, box2, box3],
+      [box4, box5, box6],
+      [box7, box8, box9],
+      [box1, box4, box7],
+      [box2, box5, box8],
+      [box3, box6, box9],
+      [box1, box5, box9],
+      [box3, box5, box7],
+    ];
+    for (let i = 0; i < winBoxes.length; i++) {
+      const winArray = winBoxes[i];
+      const allX = winArray.every((value) => value.innerHTML === "X");
+      const allO = winArray.every((value) => value.innerHTML === "O");
+      if (allX) {
+        console.log("Player 1 Wins!");
+      }
+      if (allO) {
+        console.log("Player 2 Wins!");
+      }
+    }
+  };
+  return { checkWinner };
+})();
+
+const resetGame = (function () {
+  const btn_resetBoard = document.getElementById("resetBoard");
+  const resetBoard = () => {
+    const boxes = gameBoard.boxes;
+    for (let i = 0; i < boxes.length; i++) {
+      const box = boxes[i];
+      box.innerHTML = "";
+    }
+    btn_resetBoard.addEventListener("click", resetBoard);
+  };
+  resetBoard();
+})();
 
 const displayController = (function () {
   gameBoard.markBox();
-  //   gameBoard.resetBoard();
-  //   console.log(gameBoard.test);
 })();
