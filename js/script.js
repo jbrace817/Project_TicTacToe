f2a2ce45593c8d9a9d4b9a0bde36247304cef8da
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2023-10-17 17:19:35 -0400

    updated js script with cleaned up one

--- js/script.js
+++ js/script.js
@@ -1,106 +1,150 @@
-function player(marker) {
-  return { marker: marker };
+//Factory Function for players
+function player(marker, name) {
+  return { marker: marker, name: name };
 }
 
+//Gameboard interactivity
 const gameBoard = (function () {
   const boxes = document.querySelectorAll(".box");
   const markBox = () => {
     for (let i = 0; i < boxes.length; i++) {
       const box = boxes[i];
       box.addEventListener("click", () => {
-        // if (!checkForWinner.isWinner) {
-        //   box.innerHTML = playerTurns.switchPlayers();
-        //   checkForWinner.checkWinner();
-        //   console.log(checkForWinner.isWinner);
-        // } else {
-        // }
-        if (!checkForWinner.getIsWinner()) {
-          box.innerHTML = playerTurns.switchPlayers();
-          checkForWinner.checkWinner();
-          console.log(checkForWinner.isWinner);
-        } else {
+        if (!checkForWinner.checkWinner()) {
+          box.innerHTML = playGame.takeTurns();
+          console.log(checkForWinner.checkWinner());
+          playGame.addWins();
         }
       });
     }
   };
-
   return { boxes, markBox };
 })();
 
-const playerTurns = (function () {
+//Setup and retrieve player information
+const playerInfo = (function () {
   const div_X = document.querySelector(".div-left");
   const div_O = document.querySelector(".div-right");
   const btn_X = document.querySelector(".btn-left");
   const btn_O = document.querySelector(".btn-right");
+  const playerID = document.querySelectorAll(".btn-p");
+  const getP1Name = document.querySelector(".player1Name");
+  const getP2Name = document.querySelector(".player2Name");
 
-  // let player1 = player("X");
-  let player1 = "";
-  let player2 = "";
-  let currentPlayer = player2;
-
-  btn_O.addEventListener("click", () => {
-    p1ToggleMarker();
-    whoIsPlayer1();
-  });
-  btn_X.addEventListener("click", () => {
-    p1ToggleMarker();
-    whoIsPlayer1();
-  });
-
-  const whoIsPlayer1 = () => {
-    const x = player(btn_X.firstElementChild.innerHTML);
-    const o = player(btn_O.firstElementChild.innerHTML);
-    const playerID = document.querySelectorAll(".btn-p");
+  let player1;
+  let player2;
+  const playerSetup = () => {
     if (btn_X.classList.contains("active")) {
-      player1 = x.marker;
-      player2 = o.marker;
-      playerID[0].innerHTML = "P1";
-      playerID[1].innerHTML = "P2";
-    } else {
-      player1 = o.marker;
-      player2 = x.marker;
-      playerID[0].innerHTML = "P2";
-      playerID[1].innerHTML = "P1";
-    }
-    console.log(`player1: ${player1}`);
-  };
+      player1 = player("X", getP1Name.innerHTML);
+      player2 = player("O", getP2Name.innerHTML);
 
-  const getPlayer1 = () => {
-    return player1;
+      if (player1.name === "") {
+        player1.name = "P1";
+        getP1Name.innerHTML = player1.name;
+      }
+      if (player2.name === "") {
+        player2.name = "P2";
+        getP2Name.innerHTML = player2.name;
+      }
+      playerID[0].innerHTML = player1.name;
+      playerID[1].innerHTML = player2.name;
+    } else if (btn_O.classList.contains("active")) {
+      player1 = player("O", getP1Name.innerHTML);
+      player2 = player("X", getP2Name.innerHTML);
+
+      if (player1.name === "") {
+        player1.name = "P1";
+        getP1Name.innerHTML = player1.name;
+      }
+      if (player2.name === "") {
+        player2.name = "P2";
+        getP2Name.innerHTML = player2.name;
+      }
+    }
+    console.log(player1);
   };
 
   const p1ToggleMarker = () => {
+    const playerID = document.querySelectorAll(".btn-p");
     btn_X.classList.toggle("active");
     div_X.classList.toggle("active");
     btn_O.classList.toggle("active");
     div_O.classList.toggle("active");
+    if (playerID[0].innerHTML === player1.name) {
+      playerID[1].innerHTML = player1.name;
+      playerID[0].innerHTML = player2.name;
+    } else {
+      playerID[1].innerHTML = player2.name;
+      playerID[0].innerHTML = player1.name;
+    }
   };
 
-  //Active Player Indicator
-  const playerIndicator = () => {
-    const indicator = document.querySelectorAll(".player");
-    indicator[0].classList.toggle("active");
-    indicator[1].classList.toggle("active");
+  const getPlayer1 = () => {
+    const name = player1.name;
+    const marker = player1.marker;
+
+    return { name, marker };
   };
 
-  const switchPlayers = () => {
-    whoIsPlayer1();
-    if (currentPlayer === player1) {
-      currentPlayer = player2;
-      playerIndicator();
+  const getPlayer2 = () => {
+    const name = player2.name;
+    const marker = player2.marker;
+    return { name, marker };
+  };
+
+  btn_O.addEventListener("click", () => {
+    p1ToggleMarker();
+    playerSetup();
+    console.log(getPlayer2().marker);
+  });
+  btn_X.addEventListener("click", () => {
+    p1ToggleMarker();
+    playerSetup();
+  });
+  playerSetup();
+  return { getPlayer1, getPlayer2 };
+})();
+
+//Play Game
+const playGame = (function () {
+  const activePlayer = document.querySelectorAll(".player");
+
+  const takeTurns = () => {
+    let currentPlayer;
+    if (activePlayer[0].classList.contains("active")) {
+      currentPlayer = playerInfo.getPlayer1().marker;
+      turnIndicator();
       return currentPlayer;
     } else {
-      currentPlayer = player1;
-      playerIndicator();
+      currentPlayer = playerInfo.getPlayer2().marker;
+      turnIndicator();
       return currentPlayer;
     }
   };
 
-  return { switchPlayers, getPlayer1 };
+  //Used to indicate who's turn it is.
+  const turnIndicator = () => {
+    activePlayer[0].classList.toggle("active");
+    activePlayer[1].classList.toggle("active");
+  };
+
+  const addWins = () => {
+    const p1Wins = document.querySelector(".p1Wins");
+    const p2Wins = document.querySelector(".p2Wins");
+    if (checkForWinner.checkWinner() === playerInfo.getPlayer1().name) {
+      let currentWins = Number(p1Wins.innerHTML);
+      p1Wins.innerHTML = ++currentWins;
+    } else if (checkForWinner.checkWinner() === playerInfo.getPlayer2().name) {
+      let currentWins = Number(p2Wins.innerHTML);
+      p2Wins.innerHTML = ++currentWins;
+    }
+  };
+
+  return { takeTurns, addWins };
 })();
 
+//Check for winner.
 const checkForWinner = (function () {
-  let isWinner = false;
   const checkWinner = () => {
     const [box1, box2, box3, box4, box5, box6, box7, box8, box9] =
       gameBoard.boxes;
@@ -118,76 +162,43 @@ const checkForWinner = (function () {
       const winArray = winBoxes[i];
       const allX = winArray.every((value) => value.innerHTML === "X");
       const allO = winArray.every((value) => value.innerHTML === "O");
-      const p1 = document.querySelector(".p1Wins");
-      const p2 = document.querySelector(".p2Wins");
 
-      if (playerTurns.getPlayer1() === "X") {
-        if (allX && !isWinner) {
-          console.log("Player 1 Wins!");
-          numberOfWins.addWin(p1);
-          // checkForWinner.isWinner = true;
-          isWinner = true;
-        }
-        if (allO && !isWinner) {
-          console.log("Player 2 Wins");
-          // checkForWinner.isWinner = true;
-        }
-      }
-      if (playerTurns.getPlayer1() === "O") {
-        if (allO && !isWinner) {
-          console.log("Player 1 Wins!");
-          // checkForWinner.isWinner = true;
-        }
-        if (allX && !isWinner) {
-          console.log("Player 2 Wins");
-          // checkForWinner.isWinner = true;
-        }
+      if (allX) {
+        let winner;
+        playerInfo.getPlayer1().marker === "X"
+          ? (winner = playerInfo.getPlayer1().name)
+          : (winner = playerInfo.getPlayer2().name);
+
+        return winner;
+      } else if (allO) {
+        let winner;
+        playerInfo.getPlayer1().marker === "O"
+          ? (winner = playerInfo.getPlayer1().name)
+          : (winner = playerInfo.getPlayer2().name);
+
+        return winner;
       }
     }
   };
 
-  const getIsWinner = () => {
-    return isWinner;
-  };
-  const resetWinner = () => {
-    isWinner = false;
-  };
-
-  return { checkWinner, getIsWinner, resetWinner };
+  return { checkWinner };
 })();
 
-const numberOfWins = (function () {
-  const addWin = (player) => {
-    // const p1 = document.querySelector(".p1Wins");
-    // const p2 = document.querySelector(".p2Wins");
-    if (player) {
-      let currentWins = Number(player.innerHTML);
-      player.innerHTML = currentWins + 1;
-      return;
-    }
-    // if (player == "player2") {
-    //   let currentWins = Number(p2.innerHTML);
-    //   p2.innerHTML = currentWins += 1;
-    // }
-  };
-  return { addWin };
-})();
-
+//Reset Game
 const resetGame = (function () {
   const btn_resetBoard = document.getElementById("resetBoard");
+  //ClearBoard
   const resetBoard = () => {
     const boxes = gameBoard.boxes;
     for (let i = 0; i < boxes.length; i++) {
       const box = boxes[i];
       box.innerHTML = "";
     }
-    // checkForWinner.isWinner = false;
-    // console.log(checkForWinner.isWinner);
-    // checkForWinner.resetWinner();
   };
   btn_resetBoard.addEventListener("click", resetBoard);
 })();
 
+//Display Controller
 const displayController = (function () {
   gameBoard.markBox();
 })();
